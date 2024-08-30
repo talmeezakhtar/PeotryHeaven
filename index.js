b6fa2faac9c4b165018b37c3b5823097e8353a61
Talmeez Akhtar <105080452+talmeezakhtar@users.noreply.github.com>
2024-08-30 12:44:38 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('search-btn').addEventListener('click', function() {
+    let searchInput = document.getElementById('search-input').value;
+    alert('Searching for: ' + searchInput);
+});
